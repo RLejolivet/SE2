74475e35458f53c9506cd0ca9e464a3fe5f56a49
François Reynaud <francois.p.reynaud@gmail.com>
2014-04-28 21:47:16 +0000

    ajouter la structure buffer_keyboard dans minikernel_user/keyboard.h

---
+++ minikernel_user/keyboard.h
@@ -0,0 +1,28 @@
+#ifndef KEYBOARD_H
+#define KEYBOARD_H
+#define BUFFER_SIZE 1024
+
+typedef enum bool {false, true} bool ;
+
+struct 
+{
+	char* buffer1 ;
+	char* buffer2 ;
+	char* buffer3 ;
+	char* buffer4 ;
+	
+	bool to_read1 ;
+	bool to_read2 ;
+	bool to_read3 ;
+	bool to_read4 ;
+	
+	/* tiar stands for There Is A Reader */
+	bool tiar1 ;
+	bool tiar2 ;
+	bool tiar3 ;
+	bool tiar4 ;
+
+
+	char write_to ;
+} buffer_keyboard ;
+#endif
