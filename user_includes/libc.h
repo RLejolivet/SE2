41e68788cb977d27c99b38f76097fc5a2581bccd
Raphael Lejolivet <raphael.lejolivet@gmail.com>
2014-04-29 17:22:11 +0000

    Avancée (et fin ?) du fichier libc.h auquel les utilisateurs ont accès pour écrire le code de leur processus

--- user_includes/libc.h
+++ user_includes/libc.h
@@ -20,7 +20,24 @@ int scanf(const char* format, ...);
 
 /*
  * Pauses the program's excecution for at least 'time' seconds
+ * Exact time isn't really known since commuting may not give the processor back
+ * as soon as the sleeping time is over
  */
 void sleep(int time);
+
+/*
+ * Read waits for keyboard interrupt(s), reads the typed character(s)
+ * and writes them into the buffer pointed to by result.
+ * It will read at most 'max' or 1024 characters (whichever is lower)
+ * The return value is the number of characters read
+ * The actual number of characters written in the buffer is one higher (due to '\0' added)
+ */
+int read(char* result, int max);
+
+/*
+ * Write writes the character 'input' on the vga screen,
+ * in the parameter of the current active process.
+ */
+int write(char input);
 
 #endif
