0528211592af06a4b3041e22d7e979fba26bbe3e
Raphael Lejolivet <raphael.lejolivet@gmail.com>
2014-05-12 16:03:22 +0000

    Ajout des structures tâche + presque début de leur initialisation

---
+++ include/process.h
@@ -0,0 +1,67 @@
+#ifndef PROCESS_H
+#define PROCESS_H
+
+// avant 1ere commutation : lrt (load registre rt)
+typedef struct{
+	int retour_arriere;
+	int esp_0;
+	short ss_0;
+	short null_0;
+	int esp_1;
+	short ss_1;
+	short null_1;
+	int esp_2;
+	short ss_2;
+	short null_2;
+	int cr3;
+	int eip;
+	int eflags;
+	int eax;
+	int ecx;
+	int edx;
+	int ebx;
+	int esp;
+	int ebp;
+	int esi;
+	int edi;
+	short es;
+	short null_es;
+	short cs;
+	short null_cs;
+	short ss;
+	short null_ss;
+	short ds;
+	short null_ds;
+	short fs;
+	short null_fs;
+	short gs;
+	short null_gs;
+	short ldt;
+	short null_ldt;
+	int fin;
+} __attribute__ ((packed)) tss;
+
+typedef struct{
+	tss* ptss;
+	char state; // I: inactif, R: actif, A: en attente, K: killed
+	char* buffer_read;
+	// il manque le subscreen pour les print
+	unsigned int count;
+	void* pile_u;
+	void* pile_s;
+} task_struct;
+
+extern task_struct process_0; // le process qui fait while(1);
+extern task_struct process_1;
+extern task_struct process_2;
+extern task_struct process_3;
+extern task_struct process_4;
+
+extern task_struct* current_process;
+extern task_struct* focus_process;
+
+void init_processes();
+
+
+
+#endif
---
+++ minikernel_init/process.c
@@ -0,0 +1,27 @@
+#include "process.h"
+//#include "alloc.h"
+
+task_struct process_0;
+task_struct process_1;
+task_struct process_2;
+task_struct process_3;
+task_struct process_4;
+
+task_struct* current_process = &process_1;
+task_struct* focus_process = &process_1;
+
+void init_processes(){
+	
+	/*
+	process_0.pile_s = pile_s_0;
+	process_1.pile_u = pile_u_1;
+	process_1.pile_s = pile_s_1;
+	process_2.pile_u = pile_u_2;
+	process_2.pile_s = pile_s_2;
+	process_3.pile_u = pile_u_3;
+	process_3.pile_s = pile_s_3;
+	process_4.pile_u = pile_u_4;
+	process_4.pile_s = pile_s_4;
+	*/
+
+};
